337c8159a5113d92c47408209737a6e33e1820f3
Aman-Gupta-404 <ag9139563@gmail.com>
2026-07-26 20:00:25 +0530

    Redis username and password updates

--- apps/backend/src/config/redis.config.ts
+++ apps/backend/src/config/redis.config.ts
@@ -1,6 +1,7 @@
 export const redisconfig = {
   host: process.env.REDIS_HOST,
   port: Number(process.env.REDIS_PORT),
+  username: process.env.REDIS_USERNAME || undefined,
   password: process.env.REDIS_PASSWORD || undefined,
   maxRetriesPerRequest: null,
 };
